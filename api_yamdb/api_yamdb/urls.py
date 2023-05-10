4df395e591964a8723a029eff170410e0be6ab61
Даниил Орлов <danilorlov9898@yandex.ru>
2023-05-10 15:50:51 +0300

    Delete urls

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(
@@ -14,12 +9,4 @@ urlpatterns = [
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
-    # Используется для получения access token и refresh token
-    path('api/token/',
-         TokenObtainPairView.as_view(),
-         name='token_obtain_pair'),
-    # Используется для обновления access token
-    path('api/token/refresh/',
-         TokenRefreshView.as_view(),
-         name='token_refresh'),
 ]
