32ab65afd64fe05756da1ee1ba2c1768e8d2ad4f
Даниил Орлов <danilorlov9898@yandex.ru>
2023-05-10 20:08:12 +0300

    added models

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    bio = models.TextField(
+        'Биография',
+        blank=True
+    )
+    role = models.CharField(max_length=50, default='user')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, blank=False, null=False)
+    slug = models.SlugField(max_length=50, blank=False, null=False)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, blank=False, null=False)
+    slug = models.SlugField(max_length=200, blank=False, null=False)
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=200, blank=False, null=False)
+    year = models.DateField(auto_now_add=True)
+    genre = models.ForeignKey(
+        Genre,
+        related_name='title_genre',
+        on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        Category,
+        related_name='title_category',
+        on_delete=models.SET_NULL)
+
+
+class Comment(models.Model):
+    ...
+
+
+class Review(models.Model):
+    ...
