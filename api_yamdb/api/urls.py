40fd452e0680928ecfa2712f2541c34fe9e29d59
Кутяшов Евгений <evgeniykutyashov@gmail.com>
2023-05-18 20:45:28 +0300

    fix bugs

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,11 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 from api.views import SendCodeView, SendTokenView, update_user
+from api.views import TitleViewSet, GenreViewSet, CategoryViewSet
 
 # router = SimpleRouter()
-
+router_v1 = DefaultRouter()
 # router.register('v1/users/me', UpdateUser, basename='user_update')
-
+router_v1.register('titles', TitleViewSet, basename='titles')
+router_v1.register('genres', GenreViewSet, basename='genres')
+router_v1.register('categories', CategoryViewSet, basename='categories')
 
 urlpatterns = [
     path('v1/auth/signup/', SendCodeView.as_view(), name='signup'),
