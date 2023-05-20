810f559a9422e096b862cc5cdcaee10ab3bd04d3
Даниил Орлов <danilorlov9898@yandex.ru>
2023-05-20 20:34:15 +0300

    Pytest test_users - 28/30, test_user_registration - 16/17

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,8 @@
+import re
+
 from rest_framework import serializers
 from django.core.validators import RegexValidator, MaxLengthValidator
+from rest_framework.validators import UniqueValidator
 
 
 from reviews.models import User
@@ -7,30 +10,49 @@ from reviews.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     username = serializers.CharField(
-        required=True,
-        validators=[
-            RegexValidator(
-                regex=r'^[\w.@+-]+$',
-                message=('Required. 150 characters or fewer.'
-                         'Letters, digits and @/./+/-/_ only.')
-            ),
-            MaxLengthValidator(150)
-        ])
+        required=True)
     email = serializers.EmailField(
-        required=True,
-        validators=[
-            MaxLengthValidator(254)
-        ])
+        required=True)
     first_name = serializers.CharField(
-        required=False,
-        validators=[
-            MaxLengthValidator(150)
-        ])
+        required=False)
     last_name = serializers.CharField(
-        required=False,
-        validators=[
-            MaxLengthValidator(150)
-        ])
+        required=False)
+
+    def validate_email(self, value):
+        if len(value) > 150:
+            raise serializers.ValidationError(
+                'Поле email не может быть более 254 символов!')
+        if not value:
+            raise serializers.ValidationError(
+                'Поле email не может быть пустым!')
+        return value
+
+    def validate_username(self, value):
+        if not value:
+            raise serializers.ValidationError(
+                'Поле username не может быть пустым!')
+        if len(value) > 150:
+            raise serializers.ValidationError(
+                'Поле username не может быть более 150 символов!')
+        if not re.match(r'^[\w.@+\-]*$', value):
+            raise serializers.ValidationError(
+                'Используйте буквы, цифры и символы @/./+/-/_')
+        if value == 'me':
+            raise serializers.ValidationError(
+                'Поле username не может принимать значение "me"!')
+        return value
+
+    def validate_first_name(self, value):
+        if len(value) > 150:
+            raise serializers.ValidationError(
+                'Поле first_name не может быть более 150 символов!')
+        return value
+
+    def validate_last_name(self, value):
+        if len(value) > 150:
+            raise serializers.ValidationError(
+                'Поле last_name не может быть более 150 символов!')
+        return value
 
     class Meta:
         fields = (
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,7 @@
+from django.shortcuts import get_object_or_404
 import shortuuid
-import re
 
-from rest_framework import status, permissions, viewsets, filters, serializers
+from rest_framework import status, permissions, viewsets, filters
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -17,74 +17,65 @@ from api.permissions import AdminPermission
 
 class SendCodeView(APIView):
     permission_classes = (permissions.AllowAny,)
-
-    @staticmethod
-    def validate_errors(username, email):
-        if not email or not username:
-            return {'Поле email и username не могут быть пустыми!'}
-        if len(email) > 254:
-            return {'Длина поля email не может превышать 254 символа!'}
-        if len(username) > 150:
-            return {'Длина поля username не может превышать 150 символов!'}
-        if username == 'me':
-            return {'Поле username не может принимать значение "me"!'}
-        if not re.match(r'^[\w.@+\-]*$', username):
-            return {'Используйте буквы, цифры и символы @/./+/-/_'}
+    serializer_class = UserSerializer
 
     def post(self, request):
-        email = self.request.data.get('email')
-        username = self.request.data.get('username')
-        validation_errors = self.validate_errors(username, email)
-        if validation_errors:
-            return Response(
-                validation_errors,
-                status=status.HTTP_400_BAD_REQUEST)
-        confirmation_code = shortuuid.uuid()[:6]
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            email = self.request.data.get('email')
+            username = self.request.data.get('username')
+            confirmation_code = shortuuid.uuid()[:6]
 
-        try:
-            with transaction.atomic():
-                user, created = User.objects.get_or_create(
-                    email=email,
-                    username=username)
+            try:
+                with transaction.atomic():
+                    user, created = User.objects.get_or_create(
+                        email=email,
+                        username=username)
 
-                if created:
-                    Code.objects.create(
-                        user_id=user.id,
-                        code=confirmation_code)
-                else:
-                    code_obj, code_created = Code.objects.update_or_create(
-                        user_id=user.id,
-                        defaults={
-                            'user_id': user.id,
-                            'code': confirmation_code}
+                    if created:
+                        Code.objects.create(
+                            user_id=user.id,
+                            code=confirmation_code)
+                    else:
+                        code_obj, code_created = Code.objects.update_or_create(
+                            user_id=user.id,
+                            defaults={
+                                'user_id': user.id,
+                                'code': confirmation_code}
+                        )
+
+                    send_mail(
+                        'Confirmation Code',
+                        f'Your confirmation code: {confirmation_code}',
+                        'from@example.com',
+                        [email],
+                        fail_silently=False,
                     )
 
-                send_mail(
-                    'Confirmation Code',
-                    f'Your confirmation code: {confirmation_code}',
-                    'from@example.com',
-                    [email],
-                    fail_silently=False,
-                )
+                    return Response(
+                        {'username': username, 'email': email},
+                        status=status.HTTP_200_OK
+                    )
 
+            except IntegrityError:
+                error_message = {
+                    'error':
+                    'Пользователь с таким username или email уже существует.'}
                 return Response(
-                    {'username': username, 'email': email},
-                    status=status.HTTP_200_OK
-                )
-
-        except IntegrityError:
-            error_message = {
-                'error':
-                'Пользователь с таким username или email уже существует.'}
-            return Response(error_message, status=status.HTTP_400_BAD_REQUEST)
+                    error_message,
+                    status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST)
 
 
 class SendTokenView(TokenObtainPairView):
     def post(self, request, *args, **kwargs):
         confirmation_code = request.data.get('confirmation_code')
         username = request.data.get('username')
-        code = Code.objects.filter(code=confirmation_code).first()
-        user = User.objects.filter(username=username).first()
+        code = get_object_or_404(Code, code=confirmation_code)
+        user = get_object_or_404(User, username=username)
         if not code:
             return Response({
                 'error': 'Введен неверный код подтверждения!'},
@@ -93,7 +84,7 @@ class SendTokenView(TokenObtainPairView):
         if not user:
             return Response({
                 'error': 'Такого пользователя не существует!'},
-                status=status.HTTP_404_BAD_REQUEST
+                status=status.HTTP_404_NOT_FOUND
             )
         token = AccessToken.for_user(user)
         return Response({'token': str(token)}, status=status.HTTP_200_OK)
@@ -102,7 +93,7 @@ class SendTokenView(TokenObtainPairView):
 @api_view(['GET', 'PATCH'])
 @permission_classes([permissions.IsAuthenticated])
 def update_user(request):
-    user = User.objects.get(pk=request.user.id)
+    user = get_object_or_404(User, pk=request.user.id)
     if request.method == 'PATCH':
         if 'role' in request.data:
             return Response(
@@ -127,36 +118,9 @@ class AdminCRUDUser(viewsets.ModelViewSet):
     search_fields = ('username',)
     http_method_names = ['get', 'post', 'patch', 'delete', ]
 
-    def perform_create(self, serializer):
-        username = serializer.validated_data.get('username')
-        email = serializer.validated_data.get('email')
-        if User.objects.filter(email=email).exists():
-            error_message = {'email': 'Этот email уже используется.'}
-            raise serializers.ValidationError(error_message)
-        if User.objects.filter(username=username).exists():
-            error_message = {'username': 'Этот username уже используется.'}
-            raise serializers.ValidationError(error_message)
-        serializer.save()
-
-    def delete(self, request, username):
-        user = User.objects.get(username=username)
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(
-            instance,
-            data=request.data,
-            partial=True)
-        serializer.is_valid(raise_exception=True)
-
-        role = request.data.get('role')
-        if role and role not in ['user', 'moderator', 'admin']:
-            return Response(
-                {'error': 'Недопустимая роль'},
-                status=status.HTTP_400_BAD_REQUEST)
-
-        self.perform_update(serializer)
-
-        return Response(serializer.data)
+    def create(self, request, *args, **kwargs):
+        try:
+            return super().create(request, *args, **kwargs)
+        except IntegrityError:
+            error_message = {'error': 'Этот username или email заняты.'}
+            return Response(error_message, status=status.HTTP_400_BAD_REQUEST)
